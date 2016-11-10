f4bb05b93486ddab57a15a91d5de1f00267a91cd
Yakir Vizel <yakir.vizel@gmail.com>
2016-11-10 12:02:42 -0500

    Cleanups

--- include/seahorn/HornUnroll.hh
+++ include/seahorn/HornUnroll.hh
@@ -18,23 +18,11 @@ namespace seahorn
 
 using namespace llvm;
 
-  ///
-  /// class HornUnroll implements loop unrolling in Horn clauses
-  /// HornUnroll can be used in two modes:
-  /// 1. Stand-alone. HornUnroll is invoked after HornifyModule as a LLVM 
-  /// compiler pass, unroll the loops according to user input UnrollDepth 
-  /// (command line option "horn-unroll-depth"). Then the unrolled clauses
-  /// are converted into SMT format and solved by Z3.
-  /// 2. Intergrated with class InflowChecker. The loops are unrolled 
-  /// incrementally, and certain horn rules are duplicated for self-
-  /// composition.
-  ///
+
   class HornUnroll : public llvm::ModulePass
   {
   private:
-    HornClauseDB* m_pUnrolledDB; ///< clauseDB storing the unrolled clauses in stand-alone mode.
-                          ///< TODO: this pointer is allocated but not freed at the moment. 
-                          ///< Freeing db_ptr causes several Expr to be freed twice
+    HornClauseDB* m_pUnrolledDB;
     bool inflow;  ///< marks the integrated mode.
 
     unsigned m_nBound;
@@ -49,19 +37,13 @@ using namespace llvm;
     vec_expr_type m_loopHeads;
     Rel_to_Set m_components;
 
-    expr_set_type m_toUnroll;
-
     typedef std::pair<Expr, std::vector<Expr> > exp_vec_pair;
     typedef std::map<Expr, std::vector<Expr> > expr_to_vec;
     expr_to_vec m_rel2unrolled;
 
     void unroll();
 
-    
-    /// Construct and solve the SMT query (assert safety properties)
-    /// Horn rules are converted to Z3 SMT format.
-    /// \param db should always be db_ptr, containing horn rules after reduce()
-    void solve();
+    void toZ3();
 
   public:
     static char ID;
