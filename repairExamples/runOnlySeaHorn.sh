ad7cdcce98a959a3d64f4d66e4dce976bfbf7680
Bat-Chen Rothenberg <batcheni89@gmail.com>
2021-07-02 10:16:12 +0300

    updated run scripts to new command

---
+++ repairExamples/runOnlySeaHorn.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo "current commit in repair repository: " && git rev-parse HEAD
+time sea pf "$1" --inline  --keep-temp --temp-dir=/tmp/repair/ --step=large --horn-cond-synthesis --horn-synth-cps=h1
--- repairExamples/runSynthesis.sh
+++ repairExamples/runSynthesis.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "current commit in repair repository: " && git rev-parse HEAD
-time sea pf "$1" --horn-cond-synthesis --step=small --inline && \
+time sea pf "$1" --inline  --keep-temp --temp-dir=/tmp/repair/ --step=large --horn-cond-synthesis --horn-synth-cps=h1 && \
 z3 reverse.smt2 -v:1 fp.xform.slice=false fp.xform.inline_linear=false fp.xform.inline_eager=false
 
