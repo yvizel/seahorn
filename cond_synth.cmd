6af9cc779d987f09f68bd943e985b2b634b3e14a
Yakir Vizel <yakir.vizel@gmail.com>
2021-07-06 12:25:25 +0300

    Update command file

--- cond_synth.cmd
+++ cond_synth.cmd
@@ -1,1 +1,1 @@
-sea pf $TEST --inline  --keep-temp --temp-dir=/tmp/repair/ --step=large --horn-cond-synthesis --horn-synth-cps=h1
+sea pf $TEST --inline  --keep-temp --temp-dir=/tmp/repair/ --step=large --horn-cond-synthesis --horn-synth-cps=h1 --horn-read-file
