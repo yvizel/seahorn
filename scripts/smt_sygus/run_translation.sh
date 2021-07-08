7e19556dff939e65e6048863f601817fc8d5af06
Bat-Chen Rothenberg <batcheni89@gmail.com>
2021-07-08 05:10:59 +0300

    scripts: added call to cvc4 on sl file

--- scripts/smt_sygus/run_translation.sh
+++ scripts/smt_sygus/run_translation.sh
@@ -7,5 +7,6 @@ for test in "$1"/repairExamples/tcas/versions/*; do
     cd "$DIR"
     ../../../runSynthesis.sh ../$(basename -- "$test")
     python ../../../../scripts/smt_sygus/funcdefsToSygus.py z3.out names.txt "$(basename -- "$DIR")".sl
+    cvc4 "$(basename -- "$DIR")".sl --tlimit=900000 > cvc4.out 2>&1
     cd "$CWD"
 done
