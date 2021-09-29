79d5c4bdd4ccf83d30a072e39c123b00cde3ed8d
Bat-Chen Rothenberg <batcheni89@gmail.com>
2021-09-30 00:22:43 +0300

    scripts: runSynthesis: with time

--- repairExamples/runSynthesis.sh
+++ repairExamples/runSynthesis.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "current commit in repair repository: " && git rev-parse HEAD
 time sea pf "$1" --inline  --keep-temp --temp-dir=/tmp/repair/ --step=large --horn-cond-synthesis --horn-synth-cps=h1 --horn-read-file && \
-z3 reverse.smt2 -v:1 fp.xform.slice=false fp.xform.inline_linear=false fp.xform.inline_eager=false > "$2"z3.res 2> "$2"z3.out
+{ time z3 reverse.smt2 -v:1 fp.xform.slice=false fp.xform.inline_linear=false fp.xform.inline_eager=false > "$2"z3.res 2> "$2"z3.out ; } 2> time.txt
 
