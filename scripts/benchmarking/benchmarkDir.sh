40bc894e67cd49b59e8a4d9c0f9f774a00672022
Bat-Chen Rothenberg <batcheni89@gmail.com>
2021-10-04 10:11:55 +0300

    scripts: set timeout from command line

--- scripts/benchmarking/benchmarkDir.sh
+++ scripts/benchmarking/benchmarkDir.sh
@@ -2,6 +2,7 @@
 # $1- input directory with .c files (read)
 # $2- output directory (write, should exist): a directory will be created under it.
 # $3- string that says which tool to run. options: frontend/cosyn/cvc4/all.
+# $4- timeout for all (backend) tools (in seconds).
 
 # For every file.c in $1, run tools according to $3.
 # For the results: create a directory with input_dir name, date and time, under $2.
@@ -17,6 +18,6 @@ if [ -z "${1##*/}" ]; then
 fi
 new_dir_name="$2/${1##*/}_$(date '+%d-%m-%H-%M-%S')"
 if mkdir "$new_dir_name" && ./frontend.sh "$1" "$new_dir_name/settings.out" "$new_dir_name/reverseSmt2" "$new_dir_name/forwardSl"; then
-  { [[ "$3" == "cosyn" ]] || [[ "$3" == "all" ]] ;} && ./runCosyn.sh "$new_dir_name/reverseSmt2" "$new_dir_name/settings.out" "$new_dir_name/cosyn"
-  { [[ "$3" == "cvc4" ]] || [[ "$3" == "all" ]] ;} &&  ./runCVC4.sh "$new_dir_name/forwardSl" "$new_dir_name/settings.out" "$new_dir_name/cvc4"
+  { [[ "$3" == "cosyn" ]] || [[ "$3" == "all" ]] ;} && ./runCosyn.sh "$new_dir_name/reverseSmt2" "$new_dir_name/settings.out" "$new_dir_name/cosyn" "$4"
+  { [[ "$3" == "cvc4" ]] || [[ "$3" == "all" ]] ;} &&  ./runCVC4.sh "$new_dir_name/forwardSl" "$new_dir_name/settings.out" "$new_dir_name/cvc4" "$(( 1000*$4 ))"
 fi
