32235e4696a0846e9423be8ea9e2410e1a692627
Joachim Bard <joachim.bard@cispa.de>
2021-10-18 18:24:13 +0200

    change nested test

--- spec_test/test-nested.c
+++ spec_test/test-nested.c
@@ -1,4 +1,4 @@
-/* Standard Spectre-PHT */
+/* Standard Spectre-PHT but nested */
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -7,7 +7,6 @@
 
 extern void __taint(int*);
 extern void __is_tainted(int);
-extern void spec_fence();
 extern int nd();
 
 unsigned int array1_size = 16;
@@ -16,17 +15,16 @@ uint8_t array2[256 * 512];
 uint8_t temp = 0;
 
 int main(int argn, char* args[]) {
-    int dummy = 0;
     int source;
-    int x = nd();
-    __taint(&x);
+    int x;
 
     source = nd();
+    x = nd();
     __taint(&source);
+    __taint(&x);
 
-//    spec_fence();
     if ((source < array1_size)) {
-        temp &= array2[array1[source] * 512];
+//        temp &= array2[array1[source] * 512];
         if ((x < array1_size)) {
             temp &= array2[array1[x] * 512];
         }
