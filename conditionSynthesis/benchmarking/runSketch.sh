c5f8aad990e49b8a1b2a9538688f74415ba64698
Eytan Singher <eytan.singher@gmail.com>
2021-11-04 21:14:39 +0200

    Updated run sketch to work check for sketch output

--- conditionSynthesis/benchmarking/runSketch.sh
+++ conditionSynthesis/benchmarking/runSketch.sh
@@ -12,15 +12,15 @@ echo "running sketch on file: $1"
 file_without_prefix="${1#$4}"
 mkdir -p "$2/${file_without_prefix%/*}"
 docker run --rm -v "$(realpath $4)":/host poware/sketch:1.7.6 \
-/bin/bash -c "{ time timout $3s sketch '/host/$file_without_prefix' --fe-output-dir $2 --fe-output-prog-name sketch_$(basename -- $file_without_prefix) --bnd-inbits 10 > '/host/${file_without_prefix%%.*}.sketch.res' 2> '/host/${file_without_prefix%%.*}.sketch.out' ; } 2> '/host/${file_without_prefix%%.*}.sketch.time' && \
+/bin/bash -c "{ time timout $3s sketch '/host/$file_without_prefix' --fe-output-code --fe-output-dir $2/ --fe-output-prog-name sketch_$(basename -- $file_without_prefix) --bnd-inbits 10 > '/host/${file_without_prefix%%.*}.sketch.res' 2> '/host/${file_without_prefix%%.*}.sketch.out' ; } 2> '/host/${file_without_prefix%%.*}.sketch.time' && \
 chmod a+w '/host/${file_without_prefix%%.*}.sketch.time' '/host/${file_without_prefix%%.*}.sketch.res' '/host/${file_without_prefix%%.*}.sketch.out'"
 mv $4/${file_without_prefix%%.*}.sketch.res $2/${file_without_prefix%%.*}.sketch.res #2>/dev/null
 mv $4/${file_without_prefix%%.*}.sketch.out $2/${file_without_prefix%%.*}.sketch.out #2>/dev/null
 mv $4/${file_without_prefix%%.*}.sketch.time $2/${file_without_prefix%%.*}.sketch.time #2>/dev/null
-if grep -q "unsat" "$2/${file_without_prefix%%.*}.sketch.res"; then
+if ls $2/sketch*.c 1> /dev/null 2>&1; then; then
   echo "realizable" > "$2/${file_without_prefix%%.*}.sketch.res"
-elif grep -q "sat" "$2/${file_without_prefix%%.*}.sketch.res"; then
-  echo "unrealizable" > "$2/${file_without_prefix%%.*}.sketch.res"
+# elif grep -q "sat" "$2/${file_without_prefix%%.*}.sketch.res"; then
+#   echo "unrealizable" > "$2/${file_without_prefix%%.*}.sketch.res"
 else
   cat "$2/${file_without_prefix%%.*}.sketch.res" >>  "$2/${file_without_prefix%%.*}.sketch.out"
   echo "unknown" > "$2/${file_without_prefix%%.*}.sketch.res"
