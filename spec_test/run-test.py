6b81880ee9207c2b8513b5859870d1225f62f197
Joachim Bard <joachim.bard@cispa.de>
2021-12-06 19:07:53 +0100

    Improve test script

--- spec_test/run-test.py
+++ spec_test/run-test.py
@@ -1,29 +1,35 @@
 import subprocess
 import sys
 
-# clang = "clang-10"
-# opt_level = "-O1"
-#
-# if len(sys.argv) < 2:
-#     sys.exit("Test file not provided")
-#
-# cfile = sys.argv[1]
-#
-# subprocess.run([clang, "-I../include", "-S", "-emit-llvm", opt_level, cfile], check = True)
-
+if len(sys.argv) < 2:
+    sys.exit("Test file not provided")
 llfile = sys.argv[1]
 if not llfile.endswith(".ll"):
     sys.exit("Input file must end with '.ll'")
 
 print("run on", llfile)
 subprocess.run("mkdir -p output", shell=True)
-cmd = ["../build/run/bin/sea", "horn", "--solve", "-o=output/out.smt2", "--oll=output/out.ll", "--step=large", "--horn-tail-simplifier-pve=false", "--horn-subsumption=false", "--speculative-exe", "--horn-inline-all", llfile]
+cmd = ["../build/run/bin/sea", "horn", "--solve", "-o=output/out.smt2", "--oll=output/out.ll", "--step=large", "--horn-tail-simplifier-pve=false", "--horn-subsumption=false", "--horn-inline-all", "--speculative-exe", "--insert-fences", llfile]
 p = subprocess.run(cmd, check = True, capture_output = True, text=True)
 
-fences = p.stdout.count("insert fence")
-print("number of fences:", fences)
+secure = False
+numfences = 0
+lines = p.stdout.splitlines()
+for line in lines:
+    if line.startswith("insert fence"):
+        print("  " + line)
+        numfences += 1
+    if line.startswith("Program not secure"):
+        sys.exit("  " + line)
+    if line == "unsat":
+        secure = True
+        break
 
-if p.stdout.count("unsat") != 1:
-    sys.exit("Program still not safe")
+print("  number of fences:", numfences)
+# Todo: check stderr
+#print(p.stderr)
+
+if not secure:
+    sys.exit("Program still not secure")
 
 # Todo: keep output somewhere
