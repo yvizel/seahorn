499ee36ee03cb3b804e4b8c529fb094b97b8e402
Joachim Bard <joachim.bard@cispa.de>
2022-03-17 18:06:27 +0100

    Improve test script
    Use better names for produced files and store stdout and stderr

--- spec_test/run-test.py
+++ spec_test/run-test.py
@@ -10,18 +10,24 @@ choice = sys.argv[3]
 
 if not llfile.endswith(".ll"):
     sys.exit("Input file must end with '.ll'")
+outfile = "tmp/{}_{}_{}".format(llfile[:-len('.ll')], placement, choice)
 
 print("run on", llfile, "with fences at", placement, "and", choice)
 subprocess.run("mkdir -p tmp", shell=True)
-# Todo: use better names for output files
-cmd = ["../build/run/bin/sea", "horn", "--solve", "-o=tmp/out.smt2",
-       "--oll=tmp/out.ll", "--step=large", "--horn-tail-simplifier-pve=false",
+
+cmd = ["../build/run/bin/sea", "horn", "--solve",
+       "-o={}.smt2".format(outfile),
+       "--oll={}.ll".format(outfile),
+       "--step=large", "--horn-answer", "--horn-tail-simplifier-pve=false",
        "--horn-subsumption=false", "--horn-inline-all", "--speculative-exe",
        "--insert-fences", "--fence-placement={}".format(placement),
        "--fence-choice={}".format(choice)]
+
 if len(sys.argv) > 4 and sys.argv[4] == "--in-place-training":
     cmd.append(sys.argv[4])
     print("use in-place training")
+
+cmd.append("--horn-incremental-cover=false")
 cmd.append(llfile)
 
 p = subprocess.run(cmd, check = True, capture_output = True, text=True)
@@ -31,19 +37,24 @@ numfences = 0
 lines = p.stdout.splitlines()
 for line in lines:
     if line.startswith("insert fence"):
-        print("  " + line)
+        print("    " + line)
         numfences += 1
-    if line.startswith("Program not secure"):
-        sys.exit("  " + line)
     if line == "unsat":
         secure = True
         break
 
+lines = p.stderr.splitlines()
+for line in lines:
+    if line.startswith("Program not secure"):
+        sys.exit("  " + line)
+
 print("  number of fences:", numfences)
-# Todo: check stderr
-#print(p.stderr)
+
+print(p.stdout, file=open(outfile + ".out", 'w'))
+
+# TODO: check stderr
+print(p.stderr)
+print(p.stderr, file=open(outfile + ".err", 'w'))
 
 if not secure:
     sys.exit("Program still not secure")
-
-# Todo: keep output somewhere
